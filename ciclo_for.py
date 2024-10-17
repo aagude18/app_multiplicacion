76340d1f31c8e8e06ef4fb0eba7ed4e8744378f9
aagude18 <alexagucar2022@gmail.com>
2024-10-17 16:40:15 -0500

    se actualiza ruta en el codigo

--- ciclo_for.py
+++ ciclo_for.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/multiplicacion', methods=['GET', 'POST'])
 
 def index():
     resultado = None
